b085f4fe805b5515b60c3b71d0b17817118d0dd7
Manas Tripathi <manastrips75@gmail.com>
2025-11-24 11:04:15 +0530

    Delete probstate1 directory

--- probstate1/PalindromeCheck.java
+++
@@ -1,9 +0,0 @@
-import java.util.*;
-public class PalindromeCheck {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        String s = sc.nextLine(), rev = "";
-        for (int i = s.length() - 1; i >= 0; i--) rev += s.charAt(i);
-        System.out.println(s.equalsIgnoreCase(rev) ? "Palindrome" : "Not Palindrome");
-    }
-}
