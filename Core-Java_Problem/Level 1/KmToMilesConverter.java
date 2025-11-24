e26b30bd87f427f6a924277bde4dbe7679a89209
Manas Tripathi <manastrips75@gmail.com>
2025-11-24 11:03:24 +0530

    Delete Core-Java_Problem directory

--- Core-Java_Problem/Level 1/KmToMilesConverter.java
+++
@@ -1,8 +0,0 @@
-public class KmToMilesConverter {
-    public static void main(String[] args) {
-        double distanceInKm = 10.8;
-        double milesPerKm = 0.621371; // Standard conversion factor
-        double distanceInMiles = distanceInKm * milesPerKm;
-        System.out.println("The distance " + distanceInKm + " km in miles is " + distanceInMiles);
-    }
-}
